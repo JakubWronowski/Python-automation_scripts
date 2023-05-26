1fc007af2d93b422cd031d1c40546caebbe0e85a
JakubWronowski <132200637+JakubWronowski@users.noreply.github.com>
2023-05-26 15:11:40 +0200

    Update Tortuga.py

--- Tortuga.py
+++ Tortuga.py
@@ -45,31 +45,48 @@ list_of_opponents = [
     '"Hook-Hand Hector" - His gleaming hook is a ghastly reminder of the fates he is handed to those who cross him.',
 	'"Phantom Phineas" - His spectral presence could chill the ocean’s depths, making even the bravest sailors quake.'
 ]
+list_of_weapons = {
+    "0": "Rock",
+    "1": "Cutlass",
+    "2": "Apple",
+    "3": "Wooden stick",
+    "4": "Pistol",
+}
+
 
 task_1 = input("\nWhere do you want to head? Choose wisely. Left or Right?:\n ").lower()
 
 if task_1 == "right":
-    task_2 = input("\nArgghh, Blimey!You've done it...Now another challenge.\n\
-Cross this ravine either by bridge, rope or ladder. Which path will you take?:\n ").lower()
+    task_2 = input("\nArgghh, Blimey!You've done it...Now another challenge.\nCross this ravine either by bridge, rope or ladder. Which path will you take?:\n ").lower()
     if task_2 == "rope" or task_2 == "ladder":
-        print("Next you will face one of 4 scariest and deadliest pirates on this island")
+        print("Next you will face one of 4 scariest and deadliest pirates on this island. Choose your weapon")
         random_opponent = random.choice(list_of_opponents)
-        print(random_opponent)
-        if random_opponent == list_of_opponents[1] or random_opponent == list_of_opponents[3]:
-            print("Well done! You've defeated your opponent")
-            task_4 =input("\nBy my beard! Aye, you think you clever? Let's face final quest. Only true pirate can tell the difference between gold and fake coin. Which one, left or right?\n ").lower()
-            if task_4 == "left":
-                print("Game over...")
-            else:
-                print("You win!")
-        elif random_opponent == list_of_opponents[0] or random_opponent == list_of_opponents[2]:
-            print("You've been crushed! Game over...")
-    else:
-        print("Game over...")
+        for key, value in list_of_weapons.items():
+            print(key)
+        player_choice = input("\nChoose number from 0 to 4 to reveal your weapon. Type number: \n")
+        if player_choice in list_of_weapons.keys():
+            print(f"You will be fighting with use of: {list_of_weapons[player_choice]}")
+            print()
+            input(f"Your opponent's name is {random_opponent} ")
+            if random_opponent:
+                print(random_opponent)
+                if player_choice == "0" or player_choice == "4":
+                    print("Well done! You've defeated your opponent")
+                    task_4 = input(
+                        "\nBy my beard! Aye, you think you clever? Let's face final quest. Only true pirate can tell the difference between gold and fake coin. Which one, left or right?\n ").lower()
+                    if task_4 == "left":
+                        print("Game over...")
+                    else:
+                        print("You have won the game. Congratulations!")
+
+                else:
+                     print("You've been crushed. Game over...")
+
+        else:
+            print("Invalid choice, please enter a number from 0 to 4.")
+
 else:
     print("Game over...")
-
-
 
       
     
