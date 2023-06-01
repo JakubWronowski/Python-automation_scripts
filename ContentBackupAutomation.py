4991eb6473fa09dba4ecac7df0107af25afbcce3
JakubWronowski <132200637+JakubWronowski@users.noreply.github.com>
2023-06-01 16:04:28 +0200

    Update ContentBackupAutomation.py

--- ContentBackupAutomation.py
+++ ContentBackupAutomation.py
@@ -2,7 +2,9 @@ import os
 import time
 
 def backup_data(src_folder, dst_folder):
-    
+    # expand user home directory symbol ~ if used
+    #src_folder = os.path.expanduser(src_folder)
+    #dst_folder = os.path.expanduser(dst_folder)
     if not os.path.exists(src_folder):
         raise Exception("Source folder not found")
 
@@ -21,8 +23,20 @@ def backup_data(src_folder, dst_folder):
         print(f"Backup of {src_folder} completed. Archive name: {zip_file_name}")
     else:
         print(f"Backup of {src_folder} failed.")
+    
+    #Optionally you can upload your buckup to a cloud env foe e.g. AWS S3  
+    '''s3 = boto3.client('s3')
+
+    #upload file to s3
+    try:
+        s3.upload_file(zip_file_name, bucket_name, os.path.basename(zip_file_name))
+        print(f"Backup successfully uploaded to {s3_bucket_name}")
+    except Exception as e:
+        print(f"Failed to upload backup to AWS S3: {e}")'''
+
+
 
 # replace '/path/to/directory/' with the path of the folder you want to backup
 # replace '/path/to/destination/' with the path of the folder where you want to save the backup
-backup_data('/path/to/directory/', '/path/to/destination/')
+backup_data('/path/to/directory/', '/path/to/destination/') #add s3_bucket_name if you want  
 
