8cc8dca2ac7f61870c1a6a59d8dbeb4f9b5f8794
JakubWronowski <132200637+JakubWronowski@users.noreply.github.com>
2023-06-02 14:20:20 +0200

    Add files via upload

---
+++ Python_join_SQL_datasets.py
@@ -0,0 +1,28 @@
+import os
+import pandas as pd
+import psycopg2
+
+def merge_postgresql_datasets(table_names):
+    connection_params = {
+        'host': 'localhost',
+        'port': '5432',
+        'database': 'example_databse',
+        'user': os.getenv('DB_USERNAME'),
+        'password': os.getenv('DB_PASSWORD')
+    }
+
+    conn = psycopg2.connect(**connection_params)
+    cursor = conn.cursor()
+
+    datasets = []
+    for table_name in table_names:
+        query = f"SELECT * FROM {table_name}"
+        dataset = pd.read_sql(query, conn)
+        datasets.append(dataset)
+
+    merged_dataset = pd.concat(datasets)
+    
+    conn.close()
+
+    return merged_dataset
+
